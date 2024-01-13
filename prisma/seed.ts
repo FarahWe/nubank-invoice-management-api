cf33c43816697f9d73e6af9398d6c346812b7215
Farah <eduardobfarah@gmail.com>
2024-01-13 20:02:53 -0300

    new admin seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,21 +5,20 @@ import logger from '../src/config/logger'
 const prisma = new PrismaClient()
 
 const seeder = async () => {
-  const hashPassword = await bcrypt.hash('39601210', await bcrypt.genSalt())
+  const hashPassword = await bcrypt.hash('123456', await bcrypt.genSalt())
 
   const account = await prisma.account.create({
     data: {
-      name: 'Wirdo',
-      phone: '4939601210',
+      name: 'Admin',
       password: hashPassword,
-      email: 'info@wirdo.com.br',
+      email: 'admin@admin.com',
       role: 'superuser'
     }
   })
 
   logger.info('---------user created!----------')
   logger.info(`Email: ${account.email}`)
-  logger.info(`Passwrod: 39601210`)
+  logger.info(`Passwrod: 123456`)
   logger.info('--------------------------------')
 }
 
